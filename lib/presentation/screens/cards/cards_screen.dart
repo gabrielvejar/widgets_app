0d0640eff369321ed9c20d1fe4799c71e106bcc7
gabrielvejar1 <gabriel.vejar@hfsolutions.cl>
2025-05-31 21:45:07 -0400

    Refactor CardsScreen to use a list of card data and improve layout structure

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,7 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-class CardsScreen extends StatelessWidget {
+const cards = <Map<String, dynamic>>[
+  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 5.0, 'label': 'Elevation 5'},
+];
 
+class CardsScreen extends StatelessWidget {
   static const String name = 'cards_screen';
 
   const CardsScreen({super.key});
@@ -9,10 +19,60 @@ class CardsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cards Screen'),
+      appBar: AppBar(title: const Text('Cards Screen')),
+      body: const _CardsView(),
+    );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map(
+            (card) =>
+                _CardType1(label: card['label'], elevation: card['elevation']),
+          ),
+        ],
       ),
-      body: Placeholder(),
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(label),
+
+            )
+          ],
+        ),
+      ),
     );
   }
 }
